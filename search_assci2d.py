631803ce7256439d40a5de9d11987b886eeaade6
crosage <90540469+crosage@users.noreply.github.com>
2023-06-06 21:08:56 +0800

    feat: 更新使用群聊信息发送

--- search_assci2d.py
+++ search_assci2d.py
@@ -4,6 +4,8 @@ from nonebot.params import CommandArg
 import requests
 from nonebot.adapters.onebot.v11 import Bot, Event
 from bs4 import BeautifulSoup
+from nonebot.log import logger
+from nonebot.adapters.onebot.v11 import Bot
 
 DEFAULT_HEADERS = {
     "User-Agent": (
@@ -12,14 +14,6 @@ DEFAULT_HEADERS = {
         "Chrome/99.0.4844.82 Safari/537.36"
     )
 }
-
-proxies={
-    "http": "http://127.0.0.1:7890",
-    "https": "http://127.0.0.1:7890"
-}
-
-downloadPath = "D:\\bot\\awesomebot\\awesomebot\\plugins\\imagetmp\\"
-
 
 class Ascii2dImg:
     def __init__(self, name, thumbUrl, url, source):
@@ -38,53 +32,71 @@ def dealer(data):
         if h6:
             # 图片来源
             small = h6.find("small")
-            print(small.string)
             # 图片来源链接
             links = h6.find("a")
             name = links.string
-
-            print(links["href"])
-            print(name)
-            print("******")
             return Ascii2dImg(name, thumbUrl, links["href"], small.string)
 
 
-searchImage = on_command("搜图")
+search_image = on_command("搜图")
 
 
-@searchImage.handle()
-async def searchImageHandler(event: Event, args: Message = CommandArg()):
+@search_image.handle()
+async def search_image_handler(bot: Bot, event: Event, args: Message = CommandArg()):
     _, group, qq = str(event.get_session_id()).split("_")
     ascii2d_url = "https://ascii2d.net/search/uri"
     data = {
     }
     for arg in args:
+        logger.debug(args)
         if arg.type != "image":
-            await searchImage.finish("你没有发送图片")
+            await search_image.finish("你没有发送图片")
         data.update({"uri": arg.data["url"]})
-        print(data)
         resp = requests.post(
             url=ascii2d_url,
             data=data,
-            proxies=proxies,
+            proxies={
+                "http": "http://127.0.0.1:7890",
+                "https": "http://127.0.0.1:7890"
+            },
             headers=DEFAULT_HEADERS)
         bovw = requests.get(
             resp.url.replace("color", "bovw"),
-            proxies=proxies,
+            proxies={
+                "http": "http://127.0.0.1:7890",
+                "https": "http://127.0.0.1:7890"
+            },
             headers=DEFAULT_HEADERS
         )
         print(bovw.url)
         soup = BeautifulSoup(bovw.text, features="html.parser")
         blocks = soup.find_all("div", {'class': 'row item-box'})
-        for i in blocks:
-            ascii2dImg = dealer(i)
-            if ascii2dImg == None:
-                continue
-            msg = (
-                MessageSegment.at(user_id=qq),
-                MessageSegment.image(ascii2dImg.thumbUrl),
-                MessageSegment.text("来源:" + ascii2dImg.source),
-                MessageSegment.text(ascii2dImg.url)
-            )
-            await searchImage.send(msg)
-            break
+        msgs = []
+        try:
+            cnt = 0
+            for i in blocks:
+                cnt = cnt + 1
+                if cnt == 6:
+                    break
+                ascii2dImg = dealer(i)
+                if ascii2dImg == None:
+                    continue
+
+                msg = (
+                        MessageSegment.image(ascii2dImg.thumbUrl) +
+                        MessageSegment.text("来源:" + ascii2dImg.source) +
+                        MessageSegment.text(ascii2dImg.url)
+                )
+                logger.error(type(msg))
+                tmp = {
+                    "type": "node",
+                    "data": {
+                        "name": "测试",
+                        "uin": 3408476436,
+                        "content": msg
+                    }
+                }
+                msgs.append(tmp)
+        finally:
+            await bot.send_group_forward_msg(group_id=group, messages=msgs)
+
--- search_nhentai.py
+++ search_nhentai.py
@@ -1,62 +1,103 @@
 from nonebot import on_command
-from nonebot.adapters.onebot.v11.message import Message,MessageSegment
+from nonebot.adapters.onebot.v11.message import Message, MessageSegment
 from nonebot.params import CommandArg
 import requests
-import base64
-import time
+from nonebot.adapters.onebot.v11 import Bot, Event
+from bs4 import BeautifulSoup
 from nonebot.log import logger
-from nonebot.adapters.onebot.v11 import Event
+from nonebot.adapters.onebot.v11 import Bot
 
 DEFAULT_HEADERS = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/99.0.4844.82 Safari/537.36"
-    ),
-    "authority":"soutubot.moe",
-    "accept":"application/json, text/plain, */*",
-    "origin":"https://soutubot.moe",
-    "referer":"https://soutubot.moe/",
-    "x-requested-with":"XMLHttpRequest"
+    )
 }
-proxy={
-    "http":"http://127.0.0.1:7890",
-    "https":"http://127.0.0.1:7890"
-}
-downloadPath="D:\\bot\\awesomebot\\awesomebot\\plugins\\imagetmp\\"
 
-searchHentai=on_command("搜本")
-@searchHentai.handle()
-async def searchHentaiHandler(event:Event,args:Message=CommandArg()):
-    _,group,qq=str(event.get_session_id()).split("_")
-    searchBot="https://soutubot.moe/api/search"
+class Ascii2dImg:
+    def __init__(self, name, thumbUrl, url, source):
+        self.name = name
+        self.thumbUrl = thumbUrl
+        self.url = url
+        self.source = source
+
+
+def dealer(data):
+    infos = data.find("div", {"detail-box gray-link"})
+    thumbUrl = data.find("img")
+    thumbUrl = "https://ascii2d.net" + thumbUrl["src"]
+    if infos:
+        h6 = infos.find("h6")
+        if h6:
+            # 图片来源
+            small = h6.find("small")
+            # 图片来源链接
+            links = h6.find("a")
+            name = links.string
+            return Ascii2dImg(name, thumbUrl, links["href"], small.string)
+
+
+search_image = on_command("搜图")
+
+
+@search_image.handle()
+async def search_image_handler(bot: Bot, event: Event, args: Message = CommandArg()):
+    _, group, qq = str(event.get_session_id()).split("_")
+    ascii2d_url = "https://ascii2d.net/search/uri"
+    data = {
+    }
     for arg in args:
-        if arg.type!="image":
-            await searchHentai.finish("你没有发送图片")
-        resp=requests.get(arg.data["url"],proxies=proxy)
-        h=hash(resp.content)
-        print(resp)
+        logger.debug(args)
+        if arg.type != "image":
+            await search_image.finish("你没有发送图片")
+        data.update({"uri": arg.data["url"]})
+        resp = requests.post(
+            url=ascii2d_url,
+            data=data,
+            proxies={
+                "http": "http://127.0.0.1:7890",
+                "https": "http://127.0.0.1:7890"
+            },
+            headers=DEFAULT_HEADERS)
+        bovw = requests.get(
+            resp.url.replace("color", "bovw"),
+            proxies={
+                "http": "http://127.0.0.1:7890",
+                "https": "http://127.0.0.1:7890"
+            },
+            headers=DEFAULT_HEADERS
+        )
+        print(bovw.url)
+        soup = BeautifulSoup(bovw.text, features="html.parser")
+        blocks = soup.find_all("div", {'class': 'row item-box'})
+        msgs = []
+        try:
+            cnt = 0
+            for i in blocks:
+                cnt = cnt + 1
+                if cnt == 6:
+                    break
+                ascii2dImg = dealer(i)
+                if ascii2dImg == None:
+                    continue
 
-        with open(downloadPath+str(h)+".png","w+b") as f:
-            f.write(resp.content)
-            f.close
-            Q=str(int(pow(time.time(),2))+int(pow(len(DEFAULT_HEADERS["User-Agent"]),2)))
-            encoded_data = str(base64.b64encode(Q.encode()).decode())[::-1].replace("=","")
-            fr=open(downloadPath+str(h)+".png","rb")
-            DEFAULT_HEADERS.update({"x-api-key":encoded_data})
-            data={"factor":1.2}
-            resp=requests.post(searchBot,headers=DEFAULT_HEADERS,files={"file":fr},data=data,proxies=proxy)
-            print(resp.status_code)
-            _json=resp.json()
-            url=_json["data"][0]["previewImageUrl"]
-            title=_json["data"][0]["title"]
-            page=_json["data"][0]["pagePath"]
-            msg=(
-                MessageSegment.at(user_id=qq),
-                MessageSegment.image(url),
-                MessageSegment.text("标题:"+title),
-                MessageSegment.text("https://nhentai.net/"+page)
-            )
-            await searchHentai.send(msg)
-            
-            
+                msg = (
+                        MessageSegment.at(user_id=qq) +
+                        MessageSegment.image(ascii2dImg.thumbUrl) +
+                        MessageSegment.text("来源:" + ascii2dImg.source) +
+                        MessageSegment.text(ascii2dImg.url)
+                )
+                logger.error(type(msg))
+                tmp = {
+                    "type": "node",
+                    "data": {
+                        "name": "测试",
+                        "uin": 3408476436,
+                        "content": msg
+                    }
+                }
+                msgs.append(tmp)
+        finally:
+            await bot.send_group_forward_msg(group_id=group, messages=msgs)
+
